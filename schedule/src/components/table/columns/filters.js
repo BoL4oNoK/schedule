34975bc919d0b738d2f602c8349283652bb666c3
Zheny Shnyrkevich <mogrein.dark@gmail.com>
2020-09-06 19:38:18 +0300

    fix: table size, types tags, added filter for select type

---
+++ schedule/src/components/table/columns/filters.js
@@ -0,0 +1,26 @@
+export const filtersForType = [
+  {
+    text: 'codewars',
+    value: 'codewars',
+  },
+  {
+    text: 'js task',
+    value: 'js task',
+  },
+  {
+    text: 'html task',
+    value: 'html task',
+  },
+  {
+    text: 'test',
+    value: 'test',
+  },
+  {
+    text: 'interview',
+    value: 'interview',
+  },
+  {
+    text: 'deadline',
+    value: 'deadline',
+  }
+];
