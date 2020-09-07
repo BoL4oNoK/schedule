74182e7cd23bb193472555df01552ac4d08e16e4
KirillZh <jd_senar@mail.ru>
2020-09-08 00:05:11 +0300

    feat:list added

--- schedule/src/components/App/App.jsx
+++ schedule/src/components/App/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import ScheduleList from '../list/ScheduleList'
 
 const App = () => {
-  return <h1>Schedule</h1>;
+  return <><h1>Schedule</h1>
+  <ScheduleList/>
+  </>;
 };
 
 export default App;
