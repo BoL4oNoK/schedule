b2c35cfd43b822f17fd099a9cd770099bdc97c8c
Ekaterina Latushkina <katia_sali@mail.ru>
2020-09-10 19:15:05 +0300

    feat: change function to async

--- schedule/src/components/map/map.js
+++ schedule/src/components/map/map.js
@@ -1,18 +1,19 @@
-function createMap(town, isStreet, street, house) {
-  //town название города
-  //isStreet будет select это улица, проспект, площадь или переулок
-  //street название улицы
-  //house номер дома
+import {map} from '../../constants/constants';
+
+const {
+  KEY,
+} = map;
+
+async function createMap(town, isStreet, street, house) {
   const road = street.replace(/\s+/g, '+');
-  const key = '4a07d892-7c5e-4508-8c23-e8d6632ff3d9';
-  fetch(`https://geocode-maps.yandex.ru/1.x/?format=json&apikey=${key}&geocode=${town},+${isStreet}+${road},+дом+${house}`)
-  .then((resp) => resp.json())
-  .then((data) => {
-    const location = data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos;
-    const locationReverse = location.split(' ').reverse();
-    const latitude = locationReverse[0];
-    const longitude = locationReverse[1];
-  });
+  const res = await fetch(`https://geocode-maps.yandex.ru/1.x/?format=json&apikey=${KEY}&geocode=${town},+${isStreet}+${road},+дом+${house}`);
+  const data = await res.json();
+  const location = data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(' ');
+  const [longitude, latitude] = location;
+  return {
+    longitude,
+    latitude
+  };
 }
 
 export default createMap;
--- schedule/src/constants/constants.js
+++ schedule/src/constants/constants.js
@@ -11,7 +11,12 @@ const urls = {
 
 const GIT_LINK = 'https://github.com/';
 
+const map = {
+  KEY: '4a07d892-7c5e-4508-8c23-e8d6632ff3d9',
+};
+
 export {
   GIT_LINK,
   urls,
+  map,
 };
