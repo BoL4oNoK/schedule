90566f61a63e2ec45d6d9c7b759ac5d17c0e5dc4
Anastasia Savich <anastasia.savich.s@gmail.com>
2020-09-11 21:07:38 +0300

    feat: add users to the state

---
+++ schedule/src/store/reducer/userReducer.js
@@ -0,0 +1,19 @@
+import { actionTypes } from '../actions';
+import { USERS } from '../../constants/constants';
+
+const initialState = {
+  user: USERS.student
+};
+
+export default function optionsReducer (state = initialState, action) {
+  switch (action.type) {
+    case actionTypes.CHANGE_USER: {
+      return {
+        ...state,
+        user: action.data,
+      };
+    }
+
+    default: return state;
+  }
+}
