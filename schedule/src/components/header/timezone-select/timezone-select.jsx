44cdcf6c5e8a3fed5727032db17304e3046ba259
Zheny Shnyrkevich <mogrein.dark@gmail.com>
2020-09-22 12:56:47 +0300

    fix: list description URL render

--- schedule/src/components/header/timezone-select/timezone-select.jsx
+++ schedule/src/components/header/timezone-select/timezone-select.jsx
@@ -9,7 +9,6 @@ const { Option } = Select;
 export default function TimezoneSelect() {
   const timeZone = useSelector(state => state.optionsReducer.timeZone);
   const dispatch = useDispatch();
-console.log(timeZone)
 
   const onChange = (e) => {
     dispatch(actionCreator.setTimeZone(e));
@@ -17,8 +16,12 @@ console.log(timeZone)
   }
 
 	return (
-		<Select value={timeZone} style={{ width: 175 }} onChange={onChange}>
-      {TIME_ZONES.map(zone => <Option value={zone.name} key={zone.name}>{zone.name}</Option>)}
+    <Select
+      value={timeZone}
+      style={{ width: 170 }}
+      onChange={onChange}
+    >
+      { TIME_ZONES.map(zone => <Option value={zone.name} key={zone.name}>{zone.name}</Option>) }
 		</Select>
-	)
+	);
 }
